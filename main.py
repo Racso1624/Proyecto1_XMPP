bb9f67ed8849fa866e1e2a9d1e309c8854915e50
Racso1624 <fer10_lb@outlook.com>
2023-08-14 17:18:27 -0600

    Se agrega menu

--- main.py
+++ main.py
@@ -2,3 +2,19 @@
 # Carné 20679
 # Proyecto 1
 
+print("Bienvenido al Chat")
+print("\nTienes las siguienes opciones disponibles:\n")
+print("1) Registrar una nueva cuenta")
+print("2) Iniciar sesion con una cuenta")
+print("3) Eliminar cuenta del servidor")
+print("4) Salir")
+opcion = int(input("Ingrese la opcion que desees:"))
+
+if(opcion == 1):
+    print("Creando nueva cuenta")
+elif(opcion == 2):
+    print("Ingresando sesion")
+elif(opcion == 3):
+    print("Eliminando cuenta del servidor")
+elif(opcion == 4):
+    print("Saliendo del chat")
