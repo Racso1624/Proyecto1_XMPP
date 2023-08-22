f6e3d2e7a760c07857d6c80a93c357245e1fa076
Racso1624 <fer10_lb@outlook.com>
2023-08-22 11:33:14 -0600

    Se crean funcionalidades de chat grupal

--- client_functions.py
+++ client_functions.py
@@ -41,7 +41,7 @@ async def menu(self):
         elif(opcion == 4):
             await sendMessage(self)
         elif(opcion == 5):
-            await sendMessage(self)
+            await groupchatMenu(self)
         elif(opcion == 6):
             await sendMessage(self)
 
@@ -55,9 +55,10 @@ async def groupchatMenu(self):
         opcion = int(input("Ingrese la opcion que desees:"))
 
         if(opcion == 1):
-            await showContacts(self)
+            roomname = input("Ingresa el nombre para la sala de chat: ")
+            await createChatRoom(self, roomname)
         elif(opcion == 2):
-            await addContact(self)
+            await joinChatRoom(self)
         elif(opcion == 3):
             menu_var = False
 
@@ -113,7 +114,7 @@ async def sendMessage(self):
     contact_jid = await ainput("Ingresa el JID del contacto para enviar mensaje: ")
     self.contact_chat = contact_jid
 
-    await aprint("Mensaje para", contact_jid.split("@")[0])
+    await aprint("Mensaje para: ", contact_jid.split("@")[0])
     await aprint("Si deseas salir escribe: exit chat")
 
     user_chatting = True
@@ -122,34 +123,76 @@ async def sendMessage(self):
         print(message)
         if(message == "exit chat"):
             user_chatting = False
-            self.contact_chat = ""
+            self.chat = ""
         else:
             self.send_message(mto=contact_jid, mbody=message, mtype='chat') 
 
 # Referencia de https://xmpp.org/extensions/xep-0045.html#terms-rooms
+# Referencia de https://slixmpp.readthedocs.io/en/latest/getting_started/muc.html
 # Codigo con ayuda de ChatGPT
-async def createRoom(self, name_room):
-        try:
-            self.plugin['xep_0045'].join_muc(name_room, self.boundjid.user)
+async def createChatRoom(self, name_room):
+    try:
+        print(name_room)
+        name_room = f"{name_room}@conference.alumchat.xyz"
+        print(name_room)
+        self.plugin['xep_0045'].join_muc(name_room, self.boundjid.user)
 
-            form = self.plugin['xep_0004'].make_form(ftype='submit', title='ChatRoom Configuration')
-            form['muc#roomconfig_roomname'] = name_room
-            form['muc#roomconfig_roomdesc'] = 'Sala de chat de usuario AlumChat'
-            form['muc#roomconfig_roomowners'] = [self.boundjid.user]
-            form['muc#roomconfig_maxusers'] = '50'
-            form['muc#roomconfig_publicroom'] = '1'
-            form['muc#roomconfig_persistentroom'] = '1'
-            form['muc#roomconfig_enablelogging'] = '1'
-            form['muc#roomconfig_changesubject'] = '1'
-            form['muc#roomconfig_membersonly'] = '0'
-            form['muc#roomconfig_allowinvites'] = '0'
-            form['muc#roomconfig_whois'] = 'anyone'
+        form = self.plugin['xep_0004'].make_form(ftype='submit', title='ChatRoom Configuration')
+        form['muc#roomconfig_roomname'] = name_room
+        form['muc#roomconfig_roomdesc'] = 'Sala de chat de usuario AlumChat'
+        form['muc#roomconfig_roomowners'] = [self.boundjid.user]
+        form['muc#roomconfig_maxusers'] = '50'
+        form['muc#roomconfig_publicroom'] = '1'
+        form['muc#roomconfig_persistentroom'] = '1'
+        form['muc#roomconfig_enablelogging'] = '1'
+        form['muc#roomconfig_changesubject'] = '1'
+        form['muc#roomconfig_membersonly'] = '0'
+        form['muc#roomconfig_allowinvites'] = '0'
+        form['muc#roomconfig_whois'] = 'anyone'
 
-            await self.plugin['xep_0045'].set_room_config(name_room, config=form)
-            print("Se creo la sala de chat: ", name_room)
-            self.send_message(mto=name_room, mbody="Bienvenidos a la sala: " + name_room, mtype='groupchat')
-            
-        except IqError as e:
-            print("Error para crear la sala de chat")
-        except IqTimeout:
-            print("Tiempo de espera maximo para la creacion de la sala")
+        await self.plugin['xep_0045'].set_room_config(name_room, config=form)
+        print("Se creo la sala de chat: ", name_room)
+        self.send_message(mto=name_room, mbody="Bienvenidos a la sala: " + name_room, mtype='groupchat')
+        
+    except IqError as err:
+        print("Error para crear la sala de chat")
+    except IqTimeout:
+        print("Tiempo de espera maximo para la creacion de la sala")
+
+
+# Referencia de https://xmpp.org/extensions/xep-0045.html#terms-rooms
+# Referencia de https://slixmpp.readthedocs.io/en/latest/getting_started/muc.html
+# Codigo con ayuda de ChatGPT
+async def joinChatRoom(self, name_room):
+    self.chatroom = name_room 
+    self.room_nickname = self.boundjid.user 
+
+    print("Mensajes de la sala: ")
+
+    try:
+        await self.plugin['xep_0045'].join_muc(name_room, self.room_nickname)
+    except IqError as err:
+        print(f"Error al entrar a la sala de chat: {err.iq['error']['text']}")
+    except IqTimeout:
+        print("Sin respuesta del servidor")
+        return
+
+    await aprint("Mensajes de la Sala de Chat: ", self.chatroom.split("@")[0])
+    await aprint("Para salir del chat escribe: exit chat")
+
+    user_chatting = True
+    while user_chatting:
+        message = await ainput("Escribe el mensaje: ")
+        if(message == "exit chat"):
+            user_chatting = False
+            self.chat = ""
+            exitChatRoom(self)
+        else:
+            self.send_message(self.room, message, mtype='groupchat')
+
+# Referencia de https://xmpp.org/extensions/xep-0045.html#terms-rooms
+# Referencia de https://slixmpp.readthedocs.io/en/latest/getting_started/muc.html
+def exitChatRoom(self):
+    self['xep_0045'].leave_muc(self.chatroom, self.room_nickname)
+    self.chatroom = ""
+    self.room_nickname = ""
